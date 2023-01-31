4b57ad90e7428a6afb6fd998a209c570a113e624
Alizer <saylent1@gmail.com>
2023-01-31 18:19:51 +0800

    🎨 Change dentist username display name

--- Hospital Management/Models/Dentist.cs
+++ Hospital Management/Models/Dentist.cs
@@ -7,6 +7,7 @@ namespace Hospital_Management.Models
     [Table("DentistsTable")]
     public class Dentist : IdentityUser
     {
+        [Display(Name = "Dentist Name")] 
         public string? Name { get; set; }
 
         public string? Address { get; set; }
@@ -18,6 +19,8 @@ namespace Hospital_Management.Models
         public string? CellphoneNumber { get; set; }
 
         public string? LicenseNumber { get; set; }
+
+        [Display(Name = "Dentist Name")] public string? DentistName => UserName;
 
         public string? DentistImage { get; set; }
 
